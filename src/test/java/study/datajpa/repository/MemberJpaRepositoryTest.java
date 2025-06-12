a0d13f08839372081dc5dce6496fa179b7738473
gwoprk98 <gwoprk98@gmail.com>
2025-06-12 14:34:12 +0900

    feat: 섹션5

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -26,5 +28,32 @@ class MemberJpaRepositoryTest {
         assertEquals(member.getId(), findMember.getId());
         assertEquals(member.getUsername(), findMember.getUsername());
         assertEquals(findMember, member);
+    }
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+
+        assertEquals(member1.getId(), findMember1.getId());
+        assertEquals(member2.getId(), findMember2.getId());
+
+        List<Member> all = memberJpaRepository.findAll();
+        assertEquals(all.size(), 2);
+
+        long count = memberJpaRepository.count();
+        assertEquals(count, 2);
+
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+
+        long deletedCount = memberJpaRepository.count();
+        assertEquals(deletedCount, 0);
+
     }
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -28,5 +29,32 @@ public class MemberRepositoryTest {
         assertEquals(findMember.getId(), member.getId());
         assertEquals(findMember.getUsername(), member.getUsername());
         assertEquals(findMember, member);
+    }
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        assertEquals(member1.getId(), findMember1.getId());
+        assertEquals(member2.getId(), findMember2.getId());
+
+        List<Member> all = memberRepository.findAll();
+        assertEquals(all.size(), 2);
+
+        long count = memberRepository.count();
+        assertEquals(count, 2);
+
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long deletedCount = memberRepository.count();
+        assertEquals(deletedCount, 0);
+
     }
 }
